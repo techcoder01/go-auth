e4a2a7dd487240e7d4737165f1eb44e58654d84e
Abdul Hannan <121096664+techcoder01@users.noreply.github.com>
2025-04-15 19:17:59 +0500

    updated lint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,19 +1,22 @@
 module.exports = {
-    root: true,
-    vitest: true,  // If using Vitest
-    env: {
-      node: true
-    },
-    extends: [
-      'plugin:vue/vue3-essential',
-      'eslint:recommended'
-    ],
-    parserOptions: {
-      parser: '@babel/eslint-parser',
-      requireConfigFile: false
-    },
-    rules: {
-      'vue/multi-word-component-names': 'off',
-      'no-unused-vars': 'warn'
-    }
-  }
+  root: true,
+  env: {
+    node: true,
+    "vitest-globals/env": true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    "plugin:vitest-globals/recommended",
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    ecmaVersion: 2022,
+  },
+  plugins: ['vitest'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'no-unused-vars': 'warn',
+  },
+};
